c26760967d6a7358ab253748de34d45758443ce5
takezo <taketo45@gmail.com>
2025-08-31 14:51:53 +0900

    🎨 day2 end

--- api/server.js
+++ api/server.js
@@ -7,6 +7,9 @@ const { PrismaClient } = require("@prisma/client");
 // const { PrismaClient } = require("./generated/prisma");
 // import { PrismaClient } from './generated/prisma'
 
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken");
+
 const PORT = 8888;
 
 const prisma = new PrismaClient();
@@ -20,16 +23,47 @@ app.get("/",(req, res) => {
 app.post("/api/auth/register", async (req, res) => {
   const { username, email, password } = req.body;
 
+  const hashedPass = await bcrypt.hash(password, 10);
+
   const user = await prisma.user.create({
     data: {
       username,
       email,
-      password,
+      password: hashedPass,
     },
   });
 
   return res.json({ user });
 });
+
+app.post("/api/auth/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await prisma.user.findUnique({ where: {email} });
+  if(!user){
+    return res.status(404).json({
+      error: "そのユーザーは存在しません",
+    });
+  }
+
+  const isPasswordCheck = await bcrypt.compare(password, user.password);
+
+  if(!isPasswordCheck){
+    return res.status(401).json({
+      error: "そのパスワードはまちがっています",
+    });
+  }
+
+  // token = チケットのイメージ もしくは 入場券
+  const token = jwt.sign({ id: user.id }, process.env.KEY, {
+    expiresIn: "1d",
+  });
+
+  return res.json({ token });
+
+});
+
+
+
 
 //ここでサーバーを起動させます
 app.listen(PORT, () => console.log("server start!!!!!!"));
